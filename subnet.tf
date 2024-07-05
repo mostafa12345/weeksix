0ce63a3873439a5818b9b1c59cc97490f4ba7f94
mostafa12345 <129852848+mostafa12345@users.noreply.github.com>
2024-07-05 14:01:24 +0300

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -41,3 +41,33 @@ resource "aws_subnet" "subnet-private-02b" {
     Environment = "${var.environment}"
   }
 }
+
+resource "aws_internet_gateway" "gw-01" {
+  vpc_id = aws_vpc.vpc-01.id
+
+  tags = {
+    Name = "gw-01"
+    Environment = "${var.environment}"
+  }
+}
+
+resource "aws_route_table" "rt-01" {
+  vpc_id = aws_vpc.vpc-01.id
+ route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw-01.id
+  }
+tags = {
+  Name = "rt-01"
+  Environment = "${var.environment}"
+ }
+}
+
+resource "aws_route_table_association" "art-01a" {
+ subnet_id   = aws_subnet.subnet-public-01a.id
+ route_table_id = aws_route_table.rt-01.id
+}
+resource "aws_route_table_association" "art-01b" {
+ subnet_id   = aws_subnet.subnet-public-01b.id
+ route_table_id = aws_route_table.rt-01.id
+}
